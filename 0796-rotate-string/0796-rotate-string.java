b4d983ce09d3a03ddd39fc6b428953b88565b18a
Pramod-Mengade <pramodmengade07@gmail.com>
2026-08-09 12:29:45 +0530

    Time: 1 ms (82.1%), Space: 43.3 MB (55.32%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+
+        String str = new String(s+s);
+
+        if(str.contains(goal)){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    }
+}
